2fa6a9e1a32c0eed5e84088d47a92bb265c6704b
timram <rjckec@gmail.com>
2017-07-02 12:42:17 +0300

    removes parser

--- test.py
+++
@@ -1,4 +0,0 @@
-def getAnchors(soup):
-	anchors = soup.find("div", class_="article__content").find_all("a")[1:]
-	return filter(lambda a: not(a.get("href").find("leadpages") >= 0 or 
-		a.get("href").find("11863377") >= 0), anchors)
